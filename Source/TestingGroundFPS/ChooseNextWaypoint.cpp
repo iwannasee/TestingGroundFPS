67d612136fd1ace19b63364010c6d83ad84e1d9a
DESKTOP-IO05TE8\bakem <ueto.kiei@gmail.com>
2018-08-29 22:16:22 +0900

    reading black board data in c++

--- Source/TestingGroundFPS/ChooseNextWaypoint.cpp
+++ Source/TestingGroundFPS/ChooseNextWaypoint.cpp
@@ -1,10 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
-	//Super::ExecuteTask(OwnerComp, NodeMemory);
-	UE_LOG(LogTemp, Warning, TEXT("hey bt task is running DONKEY"));
+	auto BlackBoardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackBoardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	UE_LOG(LogTemp, Warning, TEXT("%s Waypoint index: %i"), *IndexKey.SelectedKeyName.ToString(), Index);
 	return EBTNodeResult::Succeeded;
 }
